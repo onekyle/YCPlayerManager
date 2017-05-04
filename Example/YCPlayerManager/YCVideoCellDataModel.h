6e061196360edf4480d964f847a25f2bfc211f79
wangyanchao <ych.wang@outlook.com>
2017-05-04 21:18:22 +0800

    update videocell model

---
+++ Example/YCPlayerManager/YCVideoCellDataModel.h
@@ -0,0 +1,15 @@
+//
+//  YCVideoCellDataModel.h
+//  YCPlayerManager
+//
+//  Created by Durand on 4/5/17.
+//  Copyright © 2017年 ych.wang@outlook.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YCVideoCellDataModel : NSObject
+
+
++ (instancetype)modelWithDict:(NSDictionary *)dict;
+@end
---
+++ Example/YCPlayerManager/YCVideoCellDataModel.m
@@ -0,0 +1,25 @@
+//
+//  YCVideoCellDataModel.m
+//  YCPlayerManager
+//
+//  Created by Durand on 4/5/17.
+//  Copyright © 2017年 ych.wang@outlook.com. All rights reserved.
+//
+
+#import "YCVideoCellDataModel.h"
+
+@implementation YCVideoCellDataModel
++ (instancetype)modelWithDict:(NSDictionary *)dict
+{
+    id instance = [[self alloc] init];
+    if (instance) {
+        [instance setValuesForKeysWithDictionary:dict];
+    }
+    return instance;
+}
+
+- (void)setValue:(id)value forUndefinedKey:(NSString *)key
+{
+    NSLog(@"%s: find undefined key: %@ for value: %@",__func__,key,value);
+}
+@end
